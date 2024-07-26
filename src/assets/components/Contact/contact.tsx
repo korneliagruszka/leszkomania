5ead2fec97226861574ff4805404f65b715cd435
twój login do GitHuba <twj email>
2024-07-26 20:15:21 +0200

    Navigation_tbc

---
+++ src/assets/components/Contact/contact.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './contact.scss';
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <h2>Kontakt</h2>
+      <p>To jest sekcja "Kontakt".</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/assets/components/Products/products.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './products.scss';
+
+const Products: React.FC = () => {
+  return (
+    <div>
+      <h2>Moje prace</h2>
+      <p>To jest sekcja "Moje prace".</p>
+    </div>
+  );
+};
+
+export default Products;
