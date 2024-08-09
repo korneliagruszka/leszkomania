0f171355e5bb9d271beab6083bed8185f26aafec
twój login do GitHuba <twj email>
2024-08-09 20:55:40 +0200

    About_me_tbc

--- src/components/AboutMe/aboutMe.tsx
+++ src/components/AboutMe/aboutMe.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './aboutMe.scss';
+import aboutMePhoto from '../../assets/about_me_photo.jpg';
 
 const AboutMe: React.FC = () => {
   return (
-    <div>
-      <h2>O mnie</h2>
-      <p>To jest sekcja "O mnie".</p>
-      
+    <div className="about_me_container">
+      <div className="about_me_left_side">
+          <p>Witaj w świecie mojej twórczości. Jestem Leszek tworzę obrazy, rękodzieło a czasem również zdarza mi się nadać przedmiotom całkiem nowe życie. W swoją twórczość wkładam całe serce i wolny czas, pomysłów mam wiele i skrupulatnie je realizuję. Zapraszam Cię do poznania moich prac.</p>
+          <button>Moje prace</button>
+          <h2>Leszko<span>mania</span></h2>
+      </div>
+      <div className="about_me_image">
+          <img src={aboutMePhoto} alt="About Me" />
+      </div>
     </div>
+    
   );
 };
 
