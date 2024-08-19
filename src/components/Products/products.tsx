4e5ae38cf41ff8849ea76d24fd66d84f68a828ec
twój login do GitHuba <twj email>
2024-08-19 22:07:18 +0200

    Products_tbc

--- src/components/Products/products.tsx
+++ src/components/Products/products.tsx
@@ -7,21 +7,30 @@ import Renovations from '../../assets/pexels-pixabay-209235.jpg';
 const Products: React.FC = () => {
   return (
     <>
-      <h2>Poznaj moje prace</h2>
+      <h2 className="products_header">Poznaj moje prace</h2>
       <div className="products_container">
       <div className="product_item">
         <p>Obrazy
         <img src={Pictures} alt="Pictures" />
+        <button>
+          <i className="fa-solid fa-palette"></i>
+        </button>
         </p>
       </div>
       <div className="product_item">
         <p>Rękodzieło
         <img src={Handicraft} alt="Handcraft" />
+        <button>
+        <i className="fa-solid fa-hand-sparkles"></i>
+        </button>
         </p>
       </div>
       <div className="product_item">
         <p>Renowacje
         <img src={Renovations} alt="Renovations" />
+        <button>
+        <i className="fa-solid fa-hammer"></i>
+        </button>
         </p>
       </div>
     </div>
