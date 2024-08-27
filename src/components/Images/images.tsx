47d520b2e0cf1b7844bdc308b0634a9368d11dab
twój login do GitHuba <twj email>
2024-08-27 20:57:37 +0200

    Products_done

---
+++ src/components/Images/images.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './images.scss';
+
+const Images: React.FC = () => {
+    return (
+      <div>
+        <h2>Obrazy</h2>
+        <p>To jest sekcja "Obrazy".</p>
+      </div>
+    );
+  };
+  
+  export default Images;
---
+++ src/components/Renovations/renovations.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './renovations.scss';
+
+const Renovations: React.FC = () => {
+    return (
+      <div>
+        <h2>Renowacje</h2>
+        <p>To jest sekcja "Renowacje".</p>
+      </div>
+    );
+  };
+  
+  export default Renovations;
