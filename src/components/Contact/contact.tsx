a3bc7d06c783c2c7bdcff1922c5e2743ad215f7b
twój login do GitHuba <twj email>
2024-08-28 21:58:23 +0200

    Contact_tbc

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -3,9 +3,14 @@ import './contact.scss';
 
 const Contact: React.FC = () => {
   return (
-    <div>
-      <h2>Kontakt</h2>
-      <p>To jest sekcja "Kontakt".</p>
+    <div className="contact_container">
+      <h2>Leszko<span>mania</span></h2>
+      <p>Jeśli zainteresował Cię jakiś produkt, skontaktuj się ze mną.
+        <br />
+        <i className="fa-duotone fa-solid fa-phone"></i> 508 173 845
+        <br />
+        <i className="fa-sharp fa-solid fa-envelope"></i> leszek.gruszka@gmail.com
+      </p>
     </div>
   );
 };
