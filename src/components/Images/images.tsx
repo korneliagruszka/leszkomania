ab102c4202a769f5ffef0cb1368b677e4ad06400
twój login do GitHuba <twj email>
2024-09-05 13:13:55 +0200

    Images_tbc

--- src/components/Images/images.tsx
+++ src/components/Images/images.tsx
@@ -3,9 +3,10 @@ import './images.scss';
 
 const Images: React.FC = () => {
     return (
-      <div>
-        <h2>Obrazy</h2>
-        <p>To jest sekcja "Obrazy".</p>
+      <div className='images_container'>
+        <h2 className='images_header'>Autorskie obrazy</h2>
+        <div className='images_text'>
+        <p>Obrazy maluję w przypływie weny ale także na specjalne zamówienie klienta. Mogę stworzyć spersonalizowany obraz według Twoich preferencji. Oto moje prace:</p></div>
       </div>
     );
   };
