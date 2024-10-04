249ff30da0ee3d3c88923d71d6f5a45bbfaa2dd6
twój login do GitHuba <twj email>
2024-10-04 21:20:32 +0200

    Images_tbc

--- src/components/Images/images.tsx
+++ src/components/Images/images.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './images.scss';
+// import imageAbstraction from '../../assets/image_abstraction.JPG';
 
 const Images: React.FC = () => {
     return (
@@ -7,6 +8,9 @@ const Images: React.FC = () => {
         <h2 className='images_header'>Autorskie obrazy</h2>
         <div className='images_text'>
         <p>Obrazy maluję w przypływie weny ale także na specjalne zamówienie klienta. Mogę stworzyć spersonalizowany obraz według Twoich preferencji. Oto moje prace: </p></div>
+        {/* <div className='images_portfolio'>
+          <img src={imageAbstraction} alt="Image Abstraction" />
+        </div> */}
       </div>
     );
   };
