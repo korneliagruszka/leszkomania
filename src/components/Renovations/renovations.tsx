dbdf465892d271e36a0599a1cdeaaa5900c671f3
twój login do GitHuba <twj email>
2024-10-24 20:36:50 +0200

    Renovations_tbc

--- src/components/Renovations/renovations.tsx
+++ src/components/Renovations/renovations.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './renovations.scss';
-import BeforeImageTableTop from '../../assets/renovation_table_top_before.jpg';
-import AfterImageTableTop from '../../assets/renovation_table_top_done.jpg';
+import BeforeTableTop from '../../assets/renovation_table_top_before.jpg';
+import AfterTableTop from '../../assets/renovation_table_top_done.jpg';
+import BeforeTableDetails from '../../assets/renovation_table_detals.jpg';
+import AfterTableDetails from '../../assets/renovation_table_details_done.jpg';
+import BeforeTableLegs from '../../assets/renovation_table.jpg';
+import AfterTableLegs from '../../assets/renovation_table_done.jpg';
 
 const Renovations: React.FC = () => {
   const renovations = [
     {
-      before: BeforeImageTableTop,
-      after: AfterImageTableTop,
-      description: 'Blat stołu'
+      before: BeforeTableTop,
+      after: AfterTableTop,
+      description: 'Blat stolika'
     },
     {
-      before: ,
-      after: ,
-      description: ''
+      before: BeforeTableDetails,
+      after: AfterTableDetails,
+      description: 'Detale stolika'
+    },
+    {
+      before: BeforeTableLegs,
+      after: AfterTableLegs,
+      description: 'Nogi stolika'
     },
     
   ];
@@ -31,12 +40,14 @@ const Renovations: React.FC = () => {
           <div key={index} className='renovation_item'>
             <div className='renovation_images'>
               <div className='before'>
+              <p className='caption'>Przed</p>
                 <img src={renovation.before} alt='Przed renowacją' />
-                <p className='caption'>Przed</p>
+                
               </div>
               <div className='after'>
+              <p className='caption'>Po</p>
                 <img src={renovation.after} alt='Po renowacji' />
-                <p className='caption'>Po</p>
+                
               </div>
             </div>
             <p className='renovation_description'>{renovation.description}</p>
