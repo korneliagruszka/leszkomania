aba09d785b178df0e2555f7448fde3969c3ed081
twój login do GitHuba <twj email>
2024-11-15 21:03:38 +0100

    Form_logic_tbc

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
+
+console.log(process.env.EMAIL_USER, process.env.EMAIL_PASS);
+console.log('EMAIL_USER:', process.env.EMAIL_USER); // Powinno wyświetlić twój e-mail
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS); // Powinno wyświetlić twoje hasło
+
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,9 +20,7 @@ app.get('/', (req, res) => {
   });
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.wp.pl',
-    port: 465, 
-    secure: true,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USER, 
       pass: process.env.EMAIL_PASS,
@@ -26,6 +30,9 @@ const transporter = nodemailer.createTransport({
 
 app.post('/send-email', (req, res) => {
   const { email, message } = req.body;
+
+  // Dodaj logowanie, aby sprawdzić, czy dane są poprawne
+  console.log('Otrzymane dane:', { email, message });
   
   const mailOptions = {
     from: email,
@@ -36,8 +43,10 @@ app.post('/send-email', (req, res) => {
   
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
+      console.error('Błąd podczas wysyłania e-maila:', error); // Zaloguj szczegóły błędu
       return res.status(500).send(error.toString());
     }
+    console.log('Wiadomość wysłana:', info.response);
     res.status(200).send('Wiadomość wysłana pomyślnie!');
   });
   
